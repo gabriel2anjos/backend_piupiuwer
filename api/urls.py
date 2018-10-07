2e44d527efe3dc6824e255570cbf9e1de0f453e8
Gabriel dos Anjos <gabriel.anjos.dt@gmail.com>
2018-10-07 11:33:45 -0300

    fix endpoint

--- api/urls.py
+++ api/urls.py
@@ -9,7 +9,7 @@ from piu.models import *
 from api.serializers import *
 
 router = DefaultRouter()
-router.register(r'registrar', UsersViewset)
+#router.register(r'registrar', UsersViewset)
 # router.register(r'pius', PiusViewset.as_view())
 # router.register(r'usuarios',UsuariosViewset.as_view())
 
@@ -17,8 +17,8 @@ urlpatterns = [
     url(r'^login/$', obtain_jwt_token),
     url(r'^token-verify/', verify_jwt_token),
     url(r'^token-refresh/', refresh_jwt_token),
-    url(r'^usuarios', UsuariosViewset.as_view()),
-    url(r'^registrar', CreateUserView.as_view()),
+    url(r'^usuarios/$', UsuariosViewset.as_view()),
+    url(r'^usuarios/registrar/$', CreateUserView.as_view()),
     url(r'^pius/(?P<pk>\d+)', PiusRUDViewset.as_view()),
     url(r'^pius/', PiusCLView.as_view()),
     url(r'^docs/', include_docs_urls(title='PiuPiuwer')),
